0f9064f7797c98c2c94ad2f976f8c464ca2d3a99
Beuca <beuca_alexandru@yahoo.com>
2021-08-19 13:32:51 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Beuca Alexandru on 19.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
